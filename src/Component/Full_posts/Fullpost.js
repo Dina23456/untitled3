b2af944cfa19902d8841d5227f81e17bf1ba71e8
Dina23456 <dianadem1606@gmail.com>
2020-12-11 13:01:22 +0200

    h3-4

---
+++ src/Component/Full_posts/Fullpost.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Postservice from "../../service/Postservice";
+
+class Fullpost extends Component {
+    state = {post:null};
+    Postservases = new Postservice();
+
+    async componentDidMount() {
+        let {match: {params: {id}}} = this.props;
+        let post = await this.Postservases.post(id);
+        this.setState({post});
+        console.log(this.props);
+    }
+
+    render() {
+
+        let {post} = this.state;
+        return (
+            <div>
+                {post && <div>{post.id}-{post.title}-{post.body}</div>}
+            </div>
+        );
+    }
+}
+
+export default Fullpost;
--- src/Component/Post_component/Postcomponent.js
+++ src/Component/Post_component/Postcomponent.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
 
 class Postcomponent extends Component {
     render() {
-        let {item, selectThisPost}=this.props;
+        let {item, match: {URL}} = this.props
         return (
             <div>
-                    {item.id}-{item.title}
-                    <button onClick={()=>selectThisPost (item.id)}> ok </button>
+                {item.id}-{item.title} - <Link to={URL + '/' + item.id}>user details</Link>
             </div>
         );
     }
 }
 
-export default Postcomponent;
+export default withRouter(Postcomponent);
