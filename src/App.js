548e6fbc8271c8dae747433330c000d4d797fd63
Dina23456 <dianadem1606@gmail.com>
2021-01-05 17:23:54 +0200

    practice

--- src/App.js
+++ src/App.js
@@ -1,36 +1,73 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
-import AllPostComponent from "./Component/All_post/AllPostComponent";
-import Allcomments from "./Component/Allcoment/Allcomments";
+import React, {useEffect} from 'react';
+import Todolist from "./todo/Todolist";
+import Context from "./comtex/context";
+import Load from "./Load";
+import Modal from './Modal/Modal'
+const AddTodo = React.lazy(() => new Promise(resolve => {
+    setTimeout(() => {
+        resolve(import('./Add/AddTodo'))
+    }, 3000)
+}))
 
-class App extends Component {
-    render() {
+
+function App() {
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                    setLoading(false)
+                }, 2000)
+            })
+
+            }, [])
+
+        function toggleTodo(id) {
+            setTodos(todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            }))
+        }
+
+        function removeTodo(id) {
+            setTodos(todos.filter(todo => todo.id !== id))
+        }
+
+        function addTodo(title) {
+            setTodos(todos.concat([{
+                title,
+                id: Date.now(),
+                completed: false
+
+            }]))
+        }
+
         return (
+            <Context.Provider value={{removeTodo}}>
+                <div className='wraper'>
+                    <h1> REACT</h1>
+                    <Modal />
+                    <React.Suspense fallback={<Load/>}>
+                        <AddTodo onCreate={addTodo}/>
+                    </React.Suspense>
 
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to={'/posts'}>to all posts</Link>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <Link to={'/comments'}>to all comments</Link>
-                        </li>
-                    </ul>
-                    <hr/>
-                    <Switch>
-                        <Route path={'/posts'} render={() => <AllPostComponent/>}/>
-                        <Route path={'/comments'} render={() => <Allcomments/>}/>
-                    </Switch>
-                    <hr/>
+                    {loading && <Load/>}
+                    {todos.length ? <Todolist todos={todos} onToggle={toggleTodo}/> :
+                            loading ? null : (
+                                <p>No todos!</p>
+
+                        )
+                    }
                 </div>
-            </Router>
-        );
+            </Context.Provider>
+        )
+
+
     }
-}
-
-export default App;
-
+    export default App
 
